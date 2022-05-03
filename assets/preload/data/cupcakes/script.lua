59cbf55c96de5b7265294b86af042028476aede3
THEseify <104476550+THEseify@users.noreply.github.com>
2022-05-03 02:34:38 -0500

    Add files via upload

---
+++ assets/preload/data/cupcakes/script.lua
@@ -0,0 +1,84 @@
+local followchars = true;
+local xx = 920.95;
+local yy = 2600;
+local xx2 = 1500.9;
+local yy2 = 2600;
+local ofs = 50;
+local del = 0;
+local del2 = 0;
+
+function onCreate()
+    setPropertyFromClass('GameOverSubstate', 'characterName', 'bfpony');
+    setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx');
+    setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver');
+    setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd');
+end
+
+--credits to @letteryusef on twitter for the 9 lines of code below, tysm <3!
+function onCreatePost()
+    triggerEvent('Set GF Speed', 30)
+    setProperty('cameraSpeed', 2.2)
+	setProperty('healthBar.alpha', tonumber(0))
+    setProperty('iconP1.alpha', tonumber(0))
+    setProperty('iconP2.alpha', tonumber(0))
+end
+
+function onUpdate(elapsed)
+	if followchars == true then
+		if mustHitSection == false then
+		  setProperty('defaultCamZoom',0.6)
+		  if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
+			triggerEvent('Camera Follow Pos',xx-ofs,yy)
+		  end
+		  if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
+			triggerEvent('Camera Follow Pos',xx+ofs,yy)
+		  end
+		  if getProperty('dad.animation.curAnim.name') == 'singUP' then
+			triggerEvent('Camera Follow Pos',xx,yy-ofs)
+		  end
+		  if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
+			triggerEvent('Camera Follow Pos',xx,yy+ofs)
+		  end
+		  if getProperty('dad.animation.curAnim.name') == 'singLEFT-alt' then
+			triggerEvent('Camera Follow Pos',xx-ofs,yy)
+		  end
+		  if getProperty('dad.animation.curAnim.name') == 'singRIGHT-alt' then
+			triggerEvent('Camera Follow Pos',xx+ofs,yy)
+		  end
+		  if getProperty('dad.animation.curAnim.name') == 'singUP-alt' then
+			triggerEvent('Camera Follow Pos',xx,yy-ofs)
+		  end
+		  if getProperty('dad.animation.curAnim.name') == 'singDOWN-alt' then
+			triggerEvent('Camera Follow Pos',xx,yy+ofs)
+		  end
+		  if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
+			triggerEvent('Camera Follow Pos',xx,yy)
+		  end
+		  if getProperty('dad.animation.curAnim.name') == 'idle' then
+			triggerEvent('Camera Follow Pos',xx,yy)
+		  end
+		else
+		  setProperty('defaultCamZoom',0.6)
+		  if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
+			triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
+		  end
+		  if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
+			triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
+		  end
+		  if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
+			triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
+		  end
+		  if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
+			triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
+		  end
+		  if getProperty('boyfriend.animation.curAnim.name') == 'idle-alt' then
+			triggerEvent('Camera Follow Pos',xx2,yy2)
+		  end
+		  if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
+			triggerEvent('Camera Follow Pos',xx2,yy2)
+		  end
+		end
+	else
+		triggerEvent('Camera Follow Pos','','')
+	end
+end
